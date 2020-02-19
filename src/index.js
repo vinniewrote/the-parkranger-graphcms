8a3f5fda1e1cca926c839567a5111e3c9daf0e78
Vizente Freeman <vizente@thinkcerca.com>
2020-02-18 21:09:54 -0600

    first connection with grpahcms

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GRAPHCMS_API =
+  "https://api-useast.graphcms.com/v1/cjj809xoy0bn001eez7tv87fb/master";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHCMS_API }),
+  cache: new InMemoryCache()
+});
+
+const testy = client
+  .query({
+    query: gql`
+      {
+        parks {
+          name
+        }
+      }
+    `
+  })
+  .then(result => console.log(result.data.parks));
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
