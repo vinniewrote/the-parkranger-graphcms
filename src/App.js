ed82bbd3c89dae7daf41017f18a7b8858b8f79b2
Vizente Freeman <vizente@thinkcerca.com>
2020-07-24 10:37:26 -0500

    small initial updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import gql from "graphql-tag";
+
+const GRAPHCMS_API =
+  "https://api-useast.graphcms.com/v1/cjj809xoy0bn001eez7tv87fb/master";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: GRAPHCMS_API }),
+  cache: new InMemoryCache()
+});
 
 function App() {
+  const rangerparks = client
+    .query({
+      query: gql`
+        {
+          parks {
+            name
+            city
+            stateProvince
+            country
+            location {
+              latitude
+              longitude
+            }
+          }
+        }
+      `
+    })
+    .then(result => {
+      const parkquery = result.data.parks;
+      return parkquery;
+    });
+
+  console.log(rangerparks);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.keys(rangerparks).map(parks => (
+        <p>{parks.name}</p>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
-import gql from "graphql-tag";
 
 const GRAPHCMS_API =
   "https://api-useast.graphcms.com/v1/cjj809xoy0bn001eez7tv87fb/master";
@@ -16,18 +15,6 @@ const client = new ApolloClient({
   link: new HttpLink({ uri: GRAPHCMS_API }),
   cache: new InMemoryCache()
 });
-
-const testy = client
-  .query({
-    query: gql`
-      {
-        parks {
-          name
-        }
-      }
-    `
-  })
-  .then(result => console.log(result.data.parks));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
