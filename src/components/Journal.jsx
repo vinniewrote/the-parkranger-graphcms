5189479eb285a0775d3b3538f206e60a37728b6a
Vizente Freeman <vizente@thinkcerca.com>
2021-03-02 15:27:44 -0600

    journal and author creation is active

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,63 +1,86 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { request } from "graphql-request";
-
-const GRAPHCMS_API =
-  "https://api-us-east-1.graphcms.com/v2/ck8g4we3i14kb01xv6avzh80e/master";
+import { useQuery, gql, useMutation } from "@apollo/client";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export default function Journal(props, match, location) {
   const { user, isAuthenticated } = useAuth0();
 
-  const [parks, setParks] = useState(null);
-  console.log(parks);
-  useEffect(() => {
-    const fetchParks = async () => {
-      const { parks } = await request(
-        `${GRAPHCMS_API}`,
-        `
-          {
-            author(where: {email: "txcrew@txcrew.co"}) {
-              bio
-              email
-              name
-            }
-          }
-        `
-      );
+  const AUTHOR_CHECK = gql`
+    query getAuthorStatus {
+      author(where: { email: "${user.email}" }) {
+        bio
+        email
+        name
+        auth0id
+      }
+    }
+  `;
+  const CREATE_NEW_AUTHOR = gql`
+    mutation CreateNewAuthor {
+      createAuthor(data: {email: "${user.email}", name: "${user.name}", auth0id: "${user.sub}"}) {
+        auth0id
+        email
+        name
+        stage
+      }
+      publishAuthor(where: {email: "${user.email}"}, to: PUBLISHED) {
+        id
+      }
+    }
+  `;
 
-      setParks(parks);
-    };
-    fetchParks();
-  }, []);
+  const { loading, error, data } = useQuery(AUTHOR_CHECK, {
+    pollInterval: 500,
+  });
+  const [createAuthor, { authorData }] = useMutation(CREATE_NEW_AUTHOR);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data.author);
 
   return (
     <Fragment>
-      <div
-        className="ActivateJournal"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "1em 1.25em",
-          boxShadow: "0px 11px 10px 0px rgba(107,104,107,1)",
-        }}
-      >
-        <p>Please click here to activate your Park Ranger Journal</p>
-        <button
-          style={{
-            height: "3em",
-            backgroundColor: "orange",
-            border: "1px solid darkorange",
-            borderRadius: "5px",
-            alignSelf: "center",
-            cursor: "pointer",
-          }}
-        >
-          Start Journaling!
-        </button>
-      </div>
-
       <h3 style={{ marginTop: "3em" }}>Journal</h3>
-      {user.email}
+      {data.author === null ? (
+        <Fragment>
+          <div
+            className="ActivateJournal"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "1em 1.25em",
+              boxShadow: "0px 11px 10px 0px rgba(107,104,107,1)",
+            }}
+          >
+            <p>Please click here to activate your Park Ranger Journal</p>
+            <button
+              style={{
+                height: "3em",
+                backgroundColor: "orange",
+                border: "1px solid darkorange",
+                borderRadius: "5px",
+                alignSelf: "center",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                createAuthor();
+                toast("registering your journal");
+              }}
+            >
+              Start Journaling!
+            </button>
+          </div>
+
+          {/* {data.author.email} */}
+        </Fragment>
+      ) : (
+        <Fragment>
+          <div>Youre in! Start journaling</div>
+        </Fragment>
+      )}
+      <ToastContainer />
     </Fragment>
   );
 }
